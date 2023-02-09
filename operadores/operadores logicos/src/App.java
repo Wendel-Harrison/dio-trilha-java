83796b4e7476a7e2a91ca38af200eda7526ad9a2
Wendel by Windowns <wh68639@gmail.com>
2023-02-08 22:33:31 -0300

    atualizaçoes sobre o estudo java

---
+++ operadores/operadores logicos/src/App.java
@@ -0,0 +1,36 @@
+import java.sql.Date;
+
+public class App {
+    public static void main(String[] args) {
+        //String nomeCompleto = "LINGUAGEM" + " JAVA";
+        //System.out.println(nomeCompleto);
+
+        int a, b, c, d;
+
+        a = 5;
+        b = 4; 
+        c = 10;
+        d = 10;
+
+        String resultado = a==b || c==d ? "verdadeiro" : "falso";
+        System.out.println(resultado);
+
+
+
+    }
+
+}
+
+// classe Operadores .java
+
+//String nome = "Wendel";
+//int idade = 20;
+//double peso = 60.5;
+//char sexo = 'M';
+//boolean Orgao = false;
+//Date dataNascimento = new Date();
+
+//Operador Ternario =  ? : 
+
+
+
